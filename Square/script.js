cd0925d32f5265f77e1d60373da497044cdcaa09
TeaEnjoyer0 <113256556+TeaEnjoyer0@users.noreply.github.com>
2023-04-18 12:50:00 +0200

    Update script.js

--- Square/script.js
+++ Square/script.js
@@ -40,15 +40,15 @@ setInterval(function()
         x.style.height = "128px";
         x.style.backgroundColor = "#252525";
         x.style.position = "absolute";
+x.style.transition = "left 500ms, bottom 500ms, background-color 500ms";
         document.body.appendChild(x);
-        x.style.transition = "left 500ms, bottom 500ms, background-color 500ms";
-        x.style.left = Math.floor(Math.random() * 100) + "dvw";
+        x.style.left = Math.floor(Math.random() * 80) + "dvw";
         x.style.bottom = Math.floor(Math.random() * 60) + "dvh";
     
         items.push(x); 
         for(let i = 0; i < items.length; i++)
         {
-            items[i].style.left = Math.floor(Math.random() * 100) + "dvw";
+            items[i].style.left = Math.floor(Math.random() * 80) + "dvw";
             items[i].style.bottom = Math.floor(Math.random() * 60) + "dvh";
             items[i].style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
         }
