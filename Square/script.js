074b2f1a6027a14bfa9bd2c4db2d9cf07ca9ea8c
TeaEnjoyer0 <113256556+TeaEnjoyer0@users.noreply.github.com>
2023-04-18 15:41:32 +0200

    Update script.js

--- Square/script.js
+++ Square/script.js
@@ -40,18 +40,23 @@ let x;
 {
 x = document.createElement("div");
         document.body.appendChild(x);
+x.style.visibility = "hidden"
         x.style.width = "128px";
         x.style.height = "128px";
         x.style.backgroundColor = "#252525";
         x.style.position = "absolute";
 x.style.transition = "left 500ms, bottom 500ms, background-color 500ms";
-x.style.left = Math.floor(Math.random() * 60) + "dvw";
-        x.style.bottom = Math.floor(Math.random() * 60) + "dvh";
         items.push(x); 
 }
         
         for(let i = 0; i < items.length; i++)
-        {
+        {.  
+            if(i == 0)
+{
+      x.style.visibility = "visible"
+      x.style.left = Math.floor(Math.random() * 60) + "dvw";
+        x.style.bottom = Math.floor(Math.random() * 60) + "dvh";
+}
             items[i].style.left = Math.floor(Math.random() * 60) + "dvw";
             items[i].style.bottom = Math.floor(Math.random() * 60) + "dvh";
             items[i].style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
